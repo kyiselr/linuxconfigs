fab0a90d0c8a59a18bc76343665cb768b1317e90
kyiselr <93725165+kyiselr@users.noreply.github.com>
2022-04-07 21:01:07 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-mv ~/linuxconfigs/xorg.conf /etc/X11/xorg.conf
-mv ~/linuxconfigs/res.sh ~/res.sh
-mv ~/linuxconfigs/xfce4 ~/.config/xfce4
-rm -rf ~/linuxconfigs
+mv /home/user/linuxconfigs/xorg.conf /etc/X11/xorg.conf
+mv /home/user/linuxconfigs/res.sh /home/user/res.sh
+mv /home/user/linuxconfigs/xfce4 /home/user/.config/xfce4
+rm -rf /home/user/linuxconfigs
