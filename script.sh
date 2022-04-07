e6a74a3f5a87464ecba079c64a80689b18a79bab
kyiselr <93725165+kyiselr@users.noreply.github.com>
2022-04-07 21:07:53 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,9 @@
+pacman -S --noconfirm base-devel nvidia firefox
+git clone https://aur.archlinux.org/yay
+cd yay
+makepkg -si --noconfirm
+yay -S librewolf
+rmmod pcspkr
 mv /home/user/linuxconfigs/xorg.conf /etc/X11/xorg.conf
 mv /home/user/linuxconfigs/res.sh /home/user/res.sh
 mv /home/user/linuxconfigs/xfce4 /home/user/.config/xfce4
